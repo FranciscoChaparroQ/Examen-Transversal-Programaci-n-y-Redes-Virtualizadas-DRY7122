669a33cb2d2c2681d5ecfc6a2e6fbc48cfddc711
Francisco Chaparro <changeme@example.com>
2024-07-14 17:28:04 +0000

    BK Item 1 completado

---
+++ Item1.py
@@ -0,0 +1,6 @@
+# Lista de integrantes del grupo
+integrantes = ["Francisco Javier Chaparro Quintana"]
+
+# Imprimir los nombres y apellidos de los integrantes
+for integrante in integrantes:
+    print(f"Integrantes: {integrante}")
